651c3e0557e2503b4755b4bad217707fe18cc9b5
Ccaswell42 <dmitriykuzin11@yandex.ru>
2022-03-10 17:49:45 +0300

    textures

--- gnl_main.c
+++ gnl_main.c
@@ -6,7 +6,7 @@
 /*   By: ccaswell <ccaswell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 14:12:45 by ccaswell          #+#    #+#             */
-/*   Updated: 2022/03/09 18:20:14 by ccaswell         ###   ########.fr       */
+/*   Updated: 2022/03/10 17:25:49 by ccaswell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,60 @@ t_mapa *read_map(char *argv)
 	return (map_main);
 }
 
+/////
+void otrisovka(t_mapa *mapa_main)
+{
+	char **map;
+	map = mapa_main->map_data;
+
+	void	*mlx;
+	void	*img;
+	char	*relative_path = "./wals.xpm";
+	int		img_width;
+	int		img_height;
+	void	*win;
+
+	mlx = mlx_init();
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	int a = mapa_main->length;
+	int v = mapa_main->height;
+	win = mlx_new_window(mlx, 32 * a, v * 32, "Hello world!");
+
+	int x = 0;
+	int i = 0;
+	int len = (mapa_main->length);
+	while ( i < len)
+	{
+		mlx_put_image_to_window(mlx, win, img, x, 0);
+		x += 32;
+		i++;
+	}
+	i = 0;
+	x = 32;
+	len --;
+	int j = mapa_main->height;
+	while (i < (j - 1))
+	{
+		mlx_put_image_to_window(mlx, win, img, 0, x);
+		mlx_put_image_to_window(mlx, win, img, (len * 32), x);
+		x +=32;
+		i++;	
+	}
+	i = 0;
+	j = mapa_main->height;
+	x = 32;
+	j--;
+	while ( i < (len - 1))
+	{
+		mlx_put_image_to_window(mlx, win, img, x, (j * 32));
+		x += 32;
+		i++;
+	}
+	
+	mlx_loop(mlx);
+}
+/////
+
 int main(int argc, char **argv)
 {
 	if (argc < 2)
@@ -72,6 +126,7 @@ int main(int argc, char **argv)
 	map_main = read_map(argv[1]);
 
 	all_validation(argv[1], map_main);
+	otrisovka(map_main);
 
 	exit (0);
 }
--- main.c
+++ main.c
@@ -1,32 +1,45 @@
 #include "so_long.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	void	*img;
+	char	*relative_path = "./wall.xpm";
+	int		img_width;
+	int		img_height;
+	void	*win;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x0000FF00);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	win = mlx_new_window(mlx, 32 * 35, 6 * 32, "Hello world!");
+	mlx_put_image_to_window(mlx, win, img, 0, 0);
+	mlx_put_image_to_window(mlx, win, img, 0, 32);
+	mlx_put_image_to_window(mlx, win, img, 68, 70);
 	mlx_loop(mlx);
+}
+
+
+void otrisovka(t_mapa *mapa_main)
+{
+	char **map;
+	map = mapa_main->map_data;
+
+	void	*mlx;
+	void	*img;
+	char	*relative_path = "./wall.xpm";
+	int		img_width;
+	int		img_height;
+	void	*win;
+
+	mlx = mlx_init();
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	win = mlx_new_window(mlx, 32 * 35, 6 * 32, "Hello world!");
+
+	int x = 0;
+	int len = ((mapa_main->length) - 1);
+	while (map[0][len])
+	{
+		mlx_put_image_to_window(mlx, win, img, x, 0);
+		x += 32;
+	}
 }
